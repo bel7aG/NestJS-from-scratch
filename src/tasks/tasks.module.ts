fcdc82bf232691f2c199052263fabc378e7422fa
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-08-06 20:00:00 +0100

    Creating a Task Repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
-import { TasksService } from './tasks.service';
+import { Module } from '@nestjs/common'
+import { TasksController } from './tasks.controller'
+import { TasksService } from './tasks.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
+  imports: [TypeOrmModule],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
 })
 export class TasksModule {}
